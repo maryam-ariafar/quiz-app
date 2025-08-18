08663d79421c65275b5389c90418e5e08f098712
Maryam Ariafar <thisismaryamariafar@gmail.com>
2025-08-18 17:33:10 +0330

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Header from './components/Header'
 import './App.css'
-
+ 
 function App() {
 
   return (
-<div
->hi Maryam</div>
+    <Header/>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+// import logo from './assets/logo.png';
+export default function Header () {
+    return (
+        <header>
+            <h1>ReactQuiz</h1>
+            <h2>First Maryam's Project</h2>
+            {/* <img src={logo} alt="Quiz App Logo" /> */}
+        </header>
+    )
+} 
